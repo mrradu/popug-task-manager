ad393afab90fc71be3c2b7ff7003b6712b0292ea
Kirill Sumorokov <k.sumorokov@reg.ru>
2021-11-11 19:22:50 +0500

    Переделан запуск обработчика сообщений из брокера

--- inventory/consumer.py
+++ inventory/consumer.py
@@ -1,86 +1,80 @@
 import asyncio
+import os
+import signal
 from json import loads
 
 import aiojobs
 from aiokafka import AIOKafkaConsumer
+from loguru import logger
+
 from inventory.consumer_handler.performers import *
-from inventory.consumer_handler.handler import event_handler
 
 
-async def consume_be():
-    consumer = AIOKafkaConsumer(
-        "accounts",
-        bootstrap_servers="broker:9092",
-        group_id="my-group",
-        value_deserializer=lambda x: loads(x.decode("utf-8")),
-    )
-    # Get cluster layout and join group `my-group`
-    await consumer.start()
-    try:
-        print("Run consumer for business events")
-        # Consume messages
-        async for msg in consumer:
-            print(
-                "consumed: ",
-                msg.topic,
-                msg.partition,
-                msg.offset,
-                msg.key,
-                msg.value,
-                msg.timestamp,
+def attach_consumer(topic: str, group_id: str = 'my-group'):
+    def wrapper(f):
+        async def wrapped(host: str, *args, port: int = 9092, **kwargs):
+            consumer = AIOKafkaConsumer(
+                topic,
+                bootstrap_servers=f"{host}:{port}",
+                group_id=group_id,
+                value_deserializer=lambda x: loads(x.decode("utf-8")),
             )
-    except asyncio.exceptions.CancelledError:
-        print("Business events consumer: Good bye!")
-    finally:
-        # Will leave consumer group; perform autocommit if enabled.
-        await consumer.stop()
+
+            await consumer.start()
+
+            try:
+                logger.info(f"Run event consumer for topic `{topic}`")
+                await f(consumer, *args, **kwargs)
+            except asyncio.exceptions.CancelledError:
+                logger.info(f'Shutdown event consumer for topic `{topic}`')
+            finally:
+                await consumer.stop()
+
+        return wrapped
+    return wrapper
 
 
-async def consume_cud():
-    consumer = AIOKafkaConsumer(
-        "accounts-stream",
-        bootstrap_servers="broker:9092",
-        group_id="my-group",
-        value_deserializer=lambda x: loads(x.decode("utf-8")),
-    )
-    # Get cluster layout and join group `my-group`
-    await consumer.start()
-    try:
-        print("Run consumer for CUD events")
-        # Consume messages
-        async for msg in consumer:
-            print(
-                "consumed: ",
-                msg.topic,
-                msg.partition,
-                msg.offset,
-                msg.key,
-                msg.value,
-                msg.timestamp,
-            )
-    except asyncio.exceptions.CancelledError:
-        print("CUD events consumer: Good bye!")
-    finally:
-        # Will leave consumer group; perform autocommit if enabled.
-        await consumer.stop()
+@attach_consumer('accounts')
+async def consume_be(consumer: AIOKafkaConsumer):
+    async for msg in consumer:
+        logger.info(f"Consume event from topic `{msg.topic}`: {msg.value}")
+
+
+@attach_consumer('accounts-stream')
+async def consume_cud(consumer: AIOKafkaConsumer):
+    async for msg in consumer:
+        logger.info(f"Consume event from topic `{msg.topic}`: {msg.value}")
 
 
 async def main():
     scheduler = await aiojobs.create_scheduler()
 
-    await scheduler.spawn(consume_be())
-    await scheduler.spawn(consume_cud())
+    host = os.environ.get('BROKER_HOST', 'localhost')
+
+    await scheduler.spawn(consume_be(host=host))
+    await scheduler.spawn(consume_cud(host=host))
 
     return scheduler
+
+
+async def shutdown(scheduler, loop):
+    await scheduler.close()
+    loop.stop()
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
-    print("Run event consumers")
+    logger.info(f"Run event consumers")
 
     try:
         scheduler = loop.run_until_complete(main())
+
+        signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+        for s in signals:
+            loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown(scheduler, loop)))
+
         loop.run_forever()
-    except KeyboardInterrupt:
-        loop.run_until_complete(scheduler.close())
+    finally:
+        logger.info(f"Shutdown event consumers")
+        loop.close()
